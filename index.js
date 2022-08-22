88ad4aa3a3f971b52ff86a93ecbe07d0b48085f9
AyubuAyubu <ayubu6143@gmail.com>
2022-08-22 13:55:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
-const input = document.getElementById('input');
 function addingEventListener() {
-    alert('I was clicked!');
-}
-input.addEventListener('click',addingEventListener);
+    const input = document.getElementById('input')
+  
+    function clickAlert() {
+      alert('I was clicked!')
+    }
+  
+    input.addEventListener('click', clickAlert)
+  }
+  
+  addingEventListener()
